69a03bc2cf1f4b47777df9b1f2dc8f20962d5585
GAURAV RAJESH SHIRSATH <gauravshirsath911@gmail.com>
2025-12-06 13:47:05 +0530

    Add files via upload

---
+++ Day 10/script.js
@@ -0,0 +1,55 @@
+const searchBtn = document.getElementById("search-btn");
+const countryInp = document.getElementById("country-inp");
+const result = document.getElementById("result");
+
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value.trim();
+  if (countryName === "") {
+    result.innerHTML = `<h3>Please enter a country name.</h3>`;
+    return;
+  }
+
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+
+  fetch(finalURL)
+    .then((response) => response.json())
+    .then((data) => {
+      result.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+
+        <h2 class="country-name">${data[0].name.common}</h2>
+
+        <div class="data-wrapper">
+          <h4>Capital: </h4>
+          <span>${data[0].capital?.[0] || "N/A"}</span>
+        </div>
+
+        <div class="data-wrapper">
+          <h4>Continent: </h4>
+          <span>${data[0].continents[0]}</span>
+        </div>
+
+        <div class="data-wrapper">
+          <h4>Population: </h4>
+          <span>${data[0].population.toLocaleString()}</span>
+        </div>
+
+        <div class="data-wrapper">
+          <h4>Currency: </h4>
+          <span>
+            ${data[0].currencies[Object.keys(data[0].currencies)[0]].name}
+            (${Object.keys(data[0].currencies)[0]})
+          </span>
+        </div>
+
+        <div class="data-wrapper">
+          <h4>Languages: </h4>
+          <span>${Object.values(data[0].languages).join(", ")}</span>
+        </div>
+      `;
+    })
+
+    .catch(() => {
+      result.innerHTML = `<h3> Country not found. Try again.</h3>`;
+    });
+});
